8d8dd3bf8c9e1dc44b697368e3b0e8c6d96679d5
Steve <itrufeng@gmail.com>
2016-01-29 13:53:02 +0800

    创建Link类

---
+++ app/src/main/java/com/kotlinchina/smallpockets/model/Link.kt
@@ -0,0 +1,12 @@
+package com.kotlinchina.smallpockets.model
+
+import java.util.*
+
+/**
+ * Created by jizhang on 1/28/16.
+ */
+interface Link {
+    var url: String?
+    var title: String?
+    var createDate: Date?
+}
