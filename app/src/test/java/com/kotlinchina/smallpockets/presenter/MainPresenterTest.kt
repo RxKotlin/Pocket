cd2c7a42e400c38eac0bf75b9ec0f727facd98b5
iossocket <avx302@gmail.com>
2016-03-15 09:15:51 +0800

    add CheckClipboard test

--- app/src/test/java/com/kotlinchina/smallpockets/presenter/MainPresenterTest.kt
+++ app/src/test/java/com/kotlinchina/smallpockets/presenter/MainPresenterTest.kt
@@ -1,10 +1,43 @@
 package com.kotlinchina.smallpockets.presenter
 
+import android.content.Context
+import com.kotlinchina.smallpockets.presenter.impl.MainPresenter
+import com.kotlinchina.smallpockets.service.*
+import com.kotlinchina.smallpockets.view.IMainView
+import org.junit.Before
 import org.junit.Test
+import org.mockito.BDDMockito.*
 
 class MainPresenterTest {
+
+    var presenter: MainPresenter? = null
+    var mockClipboardService: ClipboardService? = null
+    var mockMainActivity: IMainView? = null
+
+
+    @Before
+    fun setUp() {
+        mockMainActivity = mock(IMainView::class.java)
+        mockClipboardService = mock(ClipboardService::class.java)
+        presenter = MainPresenter(mockMainActivity!!, mock(Context::class.java),
+                mock(HttpService::class.java), mock(StoreService::class.java),
+                mockClipboardService!!, mock(IParseDom::class.java), mock(ISaveUrlInfo::class.java))
+    }
+
+
+
     @Test
-    fun kotlinTests() {
-        assert(1 == -1)
+    fun testCheckClipboardWhenClipBoardHasValidUrl() {
+        given(mockClipboardService!!.content()).willReturn("http://google.com")
+        presenter!!.checkClipboard()
+        then(mockMainActivity!!).should(times(1)).showDialog("http://google.com");
+    }
+
+    @Test
+    fun testNotShowLinkWhenCheckClipboardHasInValidUrl() {
+        given(mockClipboardService!!.content()).willReturn("fjdklsjaklfjdkslajfklasjlk")
+        presenter!!.checkClipboard()
+        then(mockMainActivity!!).should(times(1)).showNoLinkWithMsg("Invalid String");
+        then(mockMainActivity!!).should(never()).showDialog(anyString());
     }
 }
