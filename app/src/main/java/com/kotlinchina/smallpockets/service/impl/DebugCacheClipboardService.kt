c0664c8487a4734df30b1fc000b07f05baaa3a15
Steve <itrufeng@gmail.com>
2016-04-15 10:21:16 +0800

    多一点测试数据

--- app/src/main/java/com/kotlinchina/smallpockets/service/impl/DebugCacheClipboardService.kt
+++ app/src/main/java/com/kotlinchina/smallpockets/service/impl/DebugCacheClipboardService.kt
@@ -4,6 +4,15 @@ import com.kotlinchina.smallpockets.service.ClipboardService
 
 class DebugCacheClipboardService: ClipboardService {
     override fun content(): String {
-        return "http://pinyin.sogou.com"
+        val urls = listOf(
+                "http://pinyin.sogou.com",
+                "https://jinshuju.net",
+                "http://www.a-li.com.cn",
+                "https://realm.io",
+                "http://zhihu.com",
+                "http://youku.com"
+        )
+        val random = (Math.random() * urls.count()).toInt()
+        return urls[random]
     }
 }
